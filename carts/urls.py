39b5f8bddea5a5225ed79fbf63834496d5f70a04
Aliyu Muhammad Musa <aliyumuhammad9990gmail.com>
2025-02-19 20:25:59 +0100

    day 10

--- carts/urls.py
+++ carts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('', CartAddView, name='cart_view'), 
+    path('add/<int:crop_id>/', CartAddView, name='add_to_cart'),
+    path('remove/<int:cart_id>/', RemoveFromCart, name='remove_from_cart'), 
+    path('update/<int:cart_id>/', UpdateCart, name='update_cart'), 
+    path('checkout/', CheckOut, name='checkout'), 
 ]
 
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Cart, Order
+from .models import *
 from products.models import *
 # from django.views.decorators.http import require_POST
 # from .cart import Cart
